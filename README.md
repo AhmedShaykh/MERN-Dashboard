d3c0477bcd8beba9385f99f94a207f8fdf1ccd7a
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2023-03-09 00:09:33 +0500

    Add Project

---
+++ README.md
@@ -0,0 +1,1 @@
+# Ahmëd Shäykh PortFolio!
---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from 'react';
+
+const page: FC = () => {
+    return (
+        <div>
+            <h1>
+                Ahmed Saleem Shaikh
+            </h1>
+        </div>
+    )
+};
+
+export default page;
